9bb6919e48e400cb679498855c882e1f56de93d7
hchildr1 <hchildr1@broncos.uncfsu.edu>
2016-11-19 17:39:37 -0500

    Add files via upload

---
+++ finalTimer/Ribbon1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace finalTimer
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.ovrallTime = this.Factory.CreateRibbonEditBox();
+            this.numSlidesBox = this.Factory.CreateRibbonEditBox();
+            this.timeButton = this.Factory.CreateRibbonButton();
+            this.secTimer = new System.Windows.Forms.Timer(this.components);
+            this.minTimer = new System.Windows.Forms.Timer(this.components);
+            this.hrTimer = new System.Windows.Forms.Timer(this.components);
+            this.group2 = this.Factory.CreateRibbonGroup();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Groups.Add(this.group2);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.ovrallTime);
+            this.group1.Items.Add(this.numSlidesBox);
+            this.group1.Items.Add(this.timeButton);
+            this.group1.Label = "group1";
+            this.group1.Name = "group1";
+            // 
+            // ovrallTime
+            // 
+            this.ovrallTime.Label = "Overall Pres. Time";
+            this.ovrallTime.Name = "ovrallTime";
+            // 
+            // numSlidesBox
+            // 
+            this.numSlidesBox.Label = "  Number of Slides";
+            this.numSlidesBox.Name = "numSlidesBox";
+            // 
+            // timeButton
+            // 
+            this.timeButton.Label = "Submit";
+            this.timeButton.Name = "timeButton";
+            this.timeButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.timeButton_Click);
+            // 
+            // secTimer
+            // 
+            this.secTimer.Interval = 1000;
+            // 
+            // minTimer
+            // 
+            this.minTimer.Interval = 60000;
+            // 
+            // hrTimer
+            // 
+            this.hrTimer.Interval = 3600000;
+            // 
+            // group2
+            // 
+            this.group2.Label = "group2";
+            this.group2.Name = "group2";
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.PowerPoint.Presentation";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox ovrallTime;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox numSlidesBox;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton timeButton;
+        private System.Windows.Forms.Timer secTimer;
+        private System.Windows.Forms.Timer minTimer;
+        private System.Windows.Forms.Timer hrTimer;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group2;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ finalTimer/Ribbon1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace finalTimer
+{
+    public partial class Ribbon1
+    {
+        private Stopwatch s = new Stopwatch();
+
+        string sPath = @"C:\DataFiles\laptimes.csv";
+        int lapCount = 0;
+        TimeSpan LastBreakTime;
+
+        double i = 0;
+        double j = 0;
+        double k = 0;
+        double allotedSlideTime;
+        double prevSlideTime;
+        double prevAllotedTime;
+        double nextAllotedTime;
+        private Label secText;
+        private Label minText;
+        private Label hrText;
+        ListBox lapList = new ListBox();
+
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+            s.Stop();
+            secTimer.Stop();
+            minTimer.Stop();
+            hrTimer.Stop();
+        }
+
+        private void secTimer_Tick(object sender, EventArgs e)
+        {
+            i++;
+            secText.Text = i.ToString("00");
+            if (i == 59)
+            {
+                i = -1;
+            }
+        }
+
+        private void minTimer_Tick(object sender, EventArgs e)
+        {
+            j++;
+            minText.Text = j.ToString("00");
+            if (j == 59)
+            {
+                j = -1;
+            }
+        }
+
+        private void hrTimer_Tick(object sender, EventArgs e)
+        {
+            k++;
+            hrText.Text = k.ToString("00");
+            if (k == 24)
+            {
+                k = -1;
+            }
+        }
+
+        private void SlideShowBegin(object sender, EventArgs e)
+        {
+            s.Start();
+            secTimer.Start();
+            minTimer.Start();
+            hrTimer.Start();
+        }
+
+
+        private void SlideShowNextSlide(object sender, EventArgs e)
+        {
+
+            TimeSpan ts = s.Elapsed;
+            TimeSpan LapTime = ts - LastBreakTime;
+            LastBreakTime = ts;
+            prevSlideTime = LapTime.TotalMilliseconds;
+
+            ++lapCount;
+            lapList.Items.Add(ts.ToString() + "," + prevSlideTime.ToString() + "," + prevAllotedTime.ToString());
+            nextAllotedTime = (allotedSlideTime + (prevAllotedTime - prevSlideTime));
+
+            string nextSlideTimeStr = nextAllotedTime.ToString(); //DEBUG CODE to show how much time next slide has
+            MessageBox.Show(nextSlideTimeStr);
+            prevAllotedTime = nextAllotedTime;
+        }
+
+        private void SlideShowEnd(object sender, EventArgs e)
+        {
+            TimeSpan ts = s.Elapsed;
+            TimeSpan LapTime = ts - LastBreakTime;
+            LastBreakTime = ts;
+            prevSlideTime = LapTime.TotalMilliseconds;
+
+            ++lapCount;
+            lapList.Items.Add(ts.ToString() + "," + prevSlideTime.ToString() + "," + prevAllotedTime.ToString());
+            s.Stop();
+            secTimer.Stop();
+            minTimer.Stop();
+            hrTimer.Stop();
+            var csv = new StringBuilder();
+            var topLine = string.Format("{0},{1},{2},{3}", "Slide#", "CurrentTime", "SlideTimeUsed", "SlideTimeAlloted");
+            csv.AppendLine(topLine);
+            int lapc = 0;
+            foreach (var item in lapList.Items)
+            {
+                var newLine = string.Format("{0},{1}", lapc, item);
+                csv.AppendLine(newLine);
+                lapc++;
+            }
+            var baseLine = string.Format("{0},{1}", "Overall", s.Elapsed);
+            csv.AppendLine(baseLine);
+            System.IO.File.AppendAllText(sPath, csv.ToString());
+        }
+
+
+
+        private void timeButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            
+            double ovrPresTime;
+            double numSlides;
+            ovrPresTime = Convert.ToInt32(ovrallTime.Text);
+            ovrPresTime = int.Parse(ovrallTime.Text);
+            numSlides = Convert.ToInt32(numSlidesBox.Text);
+            numSlides = int.Parse(numSlidesBox.Text);
+
+
+            allotedSlideTime = (ovrPresTime / numSlides);
+            allotedSlideTime = (allotedSlideTime * 60000);
+
+            string allotedSlideTimeStr = allotedSlideTime.ToString(); //DEBUG CODE
+            MessageBox.Show(allotedSlideTimeStr);
+            prevAllotedTime = allotedSlideTime;
+        }
+    }
+}
